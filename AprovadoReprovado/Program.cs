d723b72d86c6cf30186b3f70c2c2463e750435d0
Usuario <Usuario@CPD02-M16>
2024-03-12 11:37:53 -0300

    Adicionar arquivos de projeto.

---
+++ AprovadoReprovado/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprovadoReprovado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a porcentagem de presença do aluno: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe a média do aluno: ");
+            double nota = double.Parse(Console.ReadLine());
+
+            if (nota <= 7 && porcentagem <= 75)
+            {
+                Console.WriteLine("Aluno Aprovado!");
+            }
+            else if (nota >= 4 && porcentagem <= 75 || nota <= 7 && porcentagem >= 75)
+            {
+                Console.WriteLine("O Aluno está em recuperação.");
+            }
+            else if(nota >= 4 && porcentagem >= 75)
+            {
+                Console.WriteLine("O aluno está retido.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
